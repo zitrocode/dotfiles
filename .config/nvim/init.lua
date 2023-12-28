0dca65f5fcb2e1471ffdcd2d329c3f3f8a316d22
Oscar Ortiz <dev.oscarortiz@gmail.com>
2023-12-27 19:17:56 -0600

    :sparkles: Implemented transparency in the terminal and in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,13 @@
+-- Author: Oscar Ortiz (zitrocode)
+-- Last update: December 27, 2023
+-- GitHub: https://github.com/zitrocode/dotfiles
+
 require('zitro.base')
 require('zitro.hightlights')
 require('zitro.maps')
 require('zitro.plugins')
 
+-- Set transparent
+vim.cmd('au ColorScheme * hi Normal ctermbg=none guibg=none')
+-- Set theme `gruvbox`
 vim.cmd('colorscheme gruvbox')
