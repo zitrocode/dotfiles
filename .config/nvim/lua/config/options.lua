c31c7ef94e9d1ff89491602f428eae34aa9a9040
Ortiz Oscar <hello@ortizoscar.com>
2026-04-23 17:58:15 -0600

    refactor(options): clean legacy settings and refine option structure
    Remove deprecated settings and reorganize option comments for better readability and
    maintainability.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,84 +1,50 @@
--- Male sure to setup 'mapleader' and maplocalleader before
--- loading lazi.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 
--- Make line numbers default
-vim.opt.number = true
--- Relative line numbers, to help with jumping.
-vim.opt.relativenumber = true
+-- [[ Lines numbers ]]
+vim.opt.number = true -- Make line numbers default
+vim.opt.relativenumber = true -- Relative line numbers, to help with jumping.
 
--- Disable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = ""
+-- [[ Mouse ]]
+vim.opt.mouse = "" -- Disable mouse mode, can be useful for resizing splits for example!
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = true
+-- [[ UI behavior ]]
+vim.opt.showmode = true -- Don't show the mode, since it's already in the status line
+vim.opt.cursorline = true -- Show which line your cursor is on
+vim.opt.signcolumn = "yes:1" -- Keep signcolumn on by default
+vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor
 
--- Sync clipboard between OS and Neovim.
-vim.opt.clipboard = "unnamedplus"
+-- [[ Clipboard ]]
+vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim.
 
--- Enable break indent
-vim.opt.breakindent = true
+-- [[ Undo ]]
+vim.opt.undofile = true -- Save undo history
 
--- Save undo history
-vim.opt.undofile = true
+-- [[ Searching ]]
+vim.opt.ignorecase = true -- Ignore case with searching
+vim.opt.smartcase = true -- Override ignorecase if search contains uppercase letters
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+-- [[ Timing ]]
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time
 
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes:1"
+-- [[ Splits ]]
+vim.opt.splitright = true -- Open vertical splits to the right
+vim.opt.splitbelow = true -- Open horizontal splits below
 
--- Decrease update time
-vim.opt.updatetime = 250
+-- [[ Indentation ]]
+vim.opt.expandtab = true --Use Spaces instad of taps
+vim.opt.shiftwidth = 2 -- Size for spaces using << and >>
+vim.opt.tabstop = 2 -- Number of spaces a tab counts for
+vim.opt.softtabstop = 2 -- Number of spaces inserted when pressing tab
+vim.opt.smarttab = true -- Make tab respect shiftwidth at line start
+vim.opt.smartindent = true -- Enable smart auto indentation
+vim.opt.autoindent = true -- Copy indent form current line when starting a new one
 
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
+-- [[ Editing behavior ]]
+vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
+vim.opt.breakindent = true -- Enable break indent
 
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Set the default border for all floating windows
-vim.opt.winborder = "rounded"
-
---Use Spaces instad of taps
-vim.opt.expandtab = true
--- size for spaces using << and >>
-vim.opt.shiftwidth = 2
--- size for spaces for tab
-vim.opt.tabstop = 2
--- how many spaces when prassing tab
-vim.opt.softtabstop = 2
-
--- indetation opthions
-vim.opt.smarttab = true
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.list = true
-vim.opt.listchars = { tab = "│ ", space = " ", lead = " ", trail = "·", eol = "󰌑" }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = true
-
-vim.opt.termguicolors = true
-vim.cmd([[syntax enable]])
-vim.cmd([[filetype plugin indent on]])
+vim.opt.confirm = false -- Skip save confirmation dialogs
